0e07a72ad3438784f887987eb43b5822e6cb1fec
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-24 11:41:51 +0300

    Функция с параметром
    Функция с параметром

--- module_2_5_new.py
+++ module_2_5_new.py
@@ -2,7 +2,6 @@
 # Напишите функцию get_matrix с тремя параметрами n, m и value, которая будет
 # создавать матрицу (вложенный список) размерами n строк и m столбцов,
 # заполненную значениями value и возвращать эту матрицу в качестве результата работы.
-
 # Пункты задачи:
 # Объявите функцию get_matrix и напишите в ней параметры n, m и value.
 # Создайте пустой список matrix внутри функции get_matrix.
@@ -43,6 +42,7 @@ def get_matrix(n, m, value):
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
+
 print(result1)
 print(result2)
 print(result3)
