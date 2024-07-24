e673df8f8cae83aa662ad561382d7dad8f9dbc8d
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-24 11:41:27 +0300

    Цикл for
    Цикл for

--- module_2_4_old.py
+++ module_2_4_old.py
@@ -3,6 +3,7 @@
 list_of_cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
 # for i in list_of_cars:
 #     print('Я езжу на автомобиле марки ' + i)
+
 cars_count = 0
 for i in range(len(list_of_cars)):
     cars_count += 10
